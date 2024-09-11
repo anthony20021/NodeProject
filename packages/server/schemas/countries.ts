da6b45781d06615a4a89e0c3d493e101ead69acf
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-09-11 10:02:09 +0200

    add accesses, countries and users schema + change type by category in access.json

---
+++ packages/server/schemas/countries.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+import { ICountry } from "../types/ICountry";
+
+const countrySchema: Schema = new Schema({
+    name: { type: String, required: true, unique: true },
+    capital: { type: String, required: true, unique: true  },
+    languagesSpoken: { type: String, required: true},
+    continent: { type: String, required: true, unique: true },
+});
+
+export default mongoose.model<ICountry>('Country', countrySchema);
---
+++ packages/server/schemas/users.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "../types/IUser";
+
+const userSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+    role: { type: String, required: true}
+});
+
+export default mongoose.model<IUser>('User', userSchema);
