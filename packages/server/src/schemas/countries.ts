79db08249a6f4ca07fbaf3e2b95bd248091673c1
Anthony <anthony20021@gmail.com>
2024-09-11 16:12:28 +0200

    fix error

--- packages/server/src/schemas/countries.ts
+++ packages/server/src/schemas/countries.ts
@@ -4,7 +4,7 @@ import { ICountry } from "../types/ICountry";
 const countrySchema: Schema = new Schema({
     name: { type: String, required: true, unique: true },
     capital: { type: String, required: true, unique: true  },
-    languagesSpoken: { type: String, required: true},
+    languagesSpoken: { type: Array, required: true},
     continent: { type: String, required: true, unique: true },
 });
 
