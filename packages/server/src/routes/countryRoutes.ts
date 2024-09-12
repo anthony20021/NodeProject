999112bb95376e64a856782aa4af99d9c2626a82
Anthony <anthony20021@gmail.com>
2024-09-12 11:37:09 +0200

    verifyLogin

--- packages/server/src/routes/countryRoutes.ts
+++ packages/server/src/routes/countryRoutes.ts
@@ -8,8 +8,8 @@ const router = Router();
 
 router.get("/", Controllers.countries.get);
 router.get("/:id", Controllers.countries.where);
-router.post("/", Middlewares.validationCountries, Controllers.countries.create);
-router.delete("/:id", Controllers.countries.delete);
-router.put("/:id", Middlewares.validationCountries, Controllers.countries.update);
+router.post("/", Middlewares.validationCountries, Middlewares.isAdmin, Controllers.countries.create);
+router.delete("/:id",Middlewares.isAdmin, Controllers.countries.delete);
+router.put("/:id",Middlewares.isAdmin, Middlewares.validationCountries, Controllers.countries.update);
 
 export default router;
--- packages/server/src/routes/usersRoutes.ts
+++ packages/server/src/routes/usersRoutes.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import Controllers from "../controllers";
 import Middlewares from '../middlewares';
+import { APIResponse } from '../utils/response';
 
 const router = Router();
 
-router.get("/", Controllers.users.get);
-router.get("/:id", Controllers.users.where);
+router.get("/", Middlewares.isAdmin, Controllers.users.get);
+router.get("/:id", Middlewares.isAdmin, Controllers.users.where);
 router.post("/register", Middlewares.validationUser, Controllers.users.create);
-router.delete("/:id", Controllers.users.delete);
-router.put("/:id", Middlewares.validationUser, Controllers.users.update);
+router.delete("/:id", Middlewares.isAdmin, Controllers.users.delete); // delete un user pour les admin
+router.put("/:id", Middlewares.isAdmin, Middlewares.validationUser, Controllers.users.update); //Fonction modificacion des users pour les admin
 router.post("/login", Controllers.users.login);
 router.post("/logout", Controllers.users.logout);
+router.post("/verifyLogin", Middlewares.auth, (req, res) => {
+    APIResponse(res, true, "Login successful", 200);
+});
 
 export default router;
