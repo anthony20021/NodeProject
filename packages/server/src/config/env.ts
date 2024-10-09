8cb37196099a7046c8d61e5ad39deac3d04d4f85
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-09 14:52:59 +0200

    update env

--- packages/server/src/config/env.ts
+++ packages/server/src/config/env.ts
@@ -7,8 +7,9 @@ export const env: IEnv = {
     PORT: parseInt(process.env.PORT || "3000"),
     NODE_ENV: process.env.NODE_ENV as "development" | "production" | "test",
     JWT_SECRET: process.env.JWT_SECRET || "¡SecretSecret!",
-    REFRESH_JWT_SECRET: process.env.REFRESH_TOKEN_SECRET || "¡refreshRefresh!",
-    JWT_EXPIRATION_SECRET: process.env.JWT_EXPIRATION || "30m",
+    REFRESH_JWT_SECRET: process.env.REFRESH_JWT_SECRET || "¡refreshRefresh!",
+    JWT_EXPIRATION_SECRET: process.env.JWT_EXPIRATION_SECRET || "30m",
+    REFRESH_JWT_EXPIRATION_SECRET: process.env.REFRESH_JWT_EXPIRATION_SECRET || "1d",
     ORIGIN: process.env.ORIGIN || "http://localhost:5173",
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/nodeProject"
 };
--- packages/server/src/types/env.d.ts
+++ packages/server/src/types/env.d.ts
@@ -4,6 +4,7 @@ export interface IEnv {
     JWT_SECRET: string;
     REFRESH_JWT_SECRET: string;
     JWT_EXPIRATION_SECRET: string;
+    REFRESH_JWT_EXPIRATION_SECRET: string;
     ORIGIN: string;
     DATABASE_URL: string;
 }
