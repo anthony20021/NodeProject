64172bc874d423361009a6c8d4eb1a9eec0d2331
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-10 10:06:51 +0200

    Update countryCOntroller with logger

--- packages/server/src/controllers/countryController.ts
+++ packages/server/src/controllers/countryController.ts
@@ -1,60 +1,88 @@
 import Model from "../models/index";
-import { APIResponse } from "../utils/response";
+import { APIResponse, logger } from "../utils";
 import crypto from "crypto";
 import { Request, Response} from 'express';
 import { Types } from "mongoose";
 
 export const getAllCountries = async (request : Request, response : Response) => {
     try {
+        logger.info("[GET] - Récupère la liste de tous les pays");
+
         const countrys = await Model.country.get();
+
         APIResponse(response, countrys, "All country", 200);
     } catch (error : unknown) {
+        logger.error("Erreur lors de la récupération de la liste des pays", { error });
         APIResponse(response, error, "error", 500);
     }
-}
+};
 
 export const getCountryById = async (request : Request, response : Response) => {
     try {
+        logger.info("[GET] - Récupération d'un pays par son ID");
+
         const id = request.params.id;
         const country = await Model.country.where( new Types.ObjectId(id));
+
         if(country){
+            logger.info("Pays récupéréré");
             APIResponse(response, country, "country", 200);
         }
         else{
+            logger.warn("Pays non trouvé");
             APIResponse(response, null, "not found", 404);
         }
     } catch (error : unknown) {
+        logger.error("Erreur lors de la récupération du pays");
         APIResponse(response, error, "error", 500);
     }
-}
+};
 
 export const createCountry = async (request : Request, response : Response) => {
     try {
+        logger.info("[POST] - Création d'un nouveau pays");
+
         const newCountry = request.body;
+
         await Model.country.create(newCountry);
+
+        logger.info("Nouveau pays ajouté", { country: newCountry });
         APIResponse(response, newCountry, "country created", 200);
     } catch (error : unknown) {
+        logger.error("Erreur lors de l'ajout du pays", { error });
         APIResponse(response, error, "error", 500);
     }
-}
+};
 
 export const deleteCountry = async (request : Request, response : Response) => {
     try {
+        logger.info("[DELETE]- Suppression du pays");
+
         const id = request.params.id;
+
         await Model.country.delete(new Types.ObjectId(id));
+
+        logger.info("Pays supprimé");
         APIResponse(response, null, "country deleted", 200);
     } catch (error : unknown) {
+        logger.error("erreur lors de la suppression du pays : ", { error });
         APIResponse(response, error, "error", 500);
     }
-}
+};
 
 export const updateCountry = async (request : Request, response : Response) => {
     try {
+        logger.info("[PUT] - Mise à jour des données du pays");
+
         const id = request.params.id;
         const newCountry = request.body;
+
         await Model.country.update(new Types.ObjectId(id), newCountry);
+
+        logger.info("Données du pays mises à jour");
         APIResponse(response, newCountry, "country updated", 200);
     } catch (error : unknown) {
+        logger.error("Erreur lors de la mise à jour des données du pays", {error});
         APIResponse(response, error, "error", 500);
     }
-}
+};
