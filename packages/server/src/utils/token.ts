53bb0118f0d29ade38c31ae28077f92bd0177860
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2024-10-10 09:22:31 +0200

    token file

---
+++ packages/server/src/utils/token.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
+import { logger } from "./logger";
+
+const { JWT_SECRET, REFRESH_JWT_SECRET, JWT_EXPIRATION_SECRET, REFRESH_JWT_EXPIRATION_SECRET} = env;
+
+export const generateAccessToken = (userId: string): string => {
+    return jwt.sign({ userId }, JWT_SECRET, { expiresIn: JWT_EXPIRATION_SECRET });
+};
+
+export const generateRefreshToken = (userId: string): string => {
+    return jwt.sign({ userId }, REFRESH_JWT_SECRET, { expiresIn: REFRESH_JWT_EXPIRATION_SECRET });
+};
+
+export const verifyRefreshToken = (refreshToken: string): string | null => {
+    try {
+        const decoded = jwt.verify(refreshToken, REFRESH_JWT_SECRET) as { userId: string };
+        return decoded.userId;
+    } catch (err) {
+        logger.error("Invalide Refresh Token:" + err);
+        return null;
+    }
+};
